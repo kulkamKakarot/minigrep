bbe04972f9e3c93381d2662d06bc426d4aa9332b
kulkamKakarot <adityamacromaniax@gmail.com>
2021-06-03 14:13:04 +0530

    refactor

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,29 @@ use std::fs;
 fn main(){
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
 
-    println!("Searching for {}",query);
-    println!("In file {}",filename);
+    println!("Searching for {}",config.query);
+    println!("In file {}",config.filename);
 
-    let contents = fs::read_to_string("src/poem.txt")
+    let contents = fs::read_to_string(config.filename)
         .expect("Something wrong happened reading the file!");
 
     println!("With text:\n{}",contents);
 }
+
+struct Config{
+    query : String,
+    filename : String,
+}
+
+impl Config{
+    fn new(args: &[String]) -> Config{
+        let query = args[1].clone();
+        let filename = args[2].clone();    
+    
+        Config{query, filename}
+    }
+}
+
+    
